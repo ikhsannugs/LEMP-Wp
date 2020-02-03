ccdfecb28956b17c2afadd9606af991cbe941b05
ikhsannugs <59926991+ikhsannugs@users.noreply.github.com>
2020-02-03 13:31:39 +0700

    Update webserver-exdb.sh

--- webserver-exdb.sh
+++ webserver-exdb.sh
@@ -2,7 +2,7 @@
 sudo apt install software-properties-common
 sudo add-apt-repository -y  ppa:nginx/stable
 sudo apt-get update
-sudo apt-get install -y nginx php7.2-fpm php7.2-mysql mysql-server unzip 
+sudo apt-get install -y nginx php7.2-fpm php7.2-mysql mysql-server 
 sudo apt-get install -y php-curl php-gd php-intl php-mbstring php-soap php-xml php-xmlrpc php-zip
 
 #configure file wordpress
