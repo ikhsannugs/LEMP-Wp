4a2c13975e9b5db2091e8e07a706917cc1da1ae6
ikhsannugs <59926991+ikhsannugs@users.noreply.github.com>
2020-02-03 09:07:54 +0700

    Update webserver.sh

--- webserver.sh
+++ webserver.sh
@@ -4,4 +4,7 @@ sudo add-apt-repository -y  ppa:nginx/stable
 sudo apt-get update
 sudo apt-get install -y nginx php7.2-fpm php7.2-mysql mysql-server 
 sudo apt-get install -y php-curl php-gd php-intl php-mbstring php-soap php-xml php-xmlrpc php-zip
+curl -LO https://wordpress.org/latest.tar.gz
+sudo tar xzvf /latest.tar.gz
+curl -s https://api.wordpress.org/secret-key/1.1/salt/ >> /text
        
