630b14810c74fa5f8fd185ee4775cdd663e92395
ikhsannugs <59926991+ikhsannugs@users.noreply.github.com>
2020-02-12 01:20:06 +0700

    Update webserver-db.sh

--- webserver-db.sh
+++ webserver-db.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-sudo apt install software-properties-common
-sudo add-apt-repository -y  ppa:nginx/stable
-sudo apt-get update
-sudo apt-get install -y nginx php7.2-fpm php7.2-mysql mysql-server
-sudo apt-get install -y php-curl php-gd php-intl php-mbstring php-soap php-xml php-xmlrpc php-zip
+apt install software-properties-common
+add-apt-repository -y  ppa:nginx/stable
+apt-get update
+apt-get install -y nginx php7.2-fpm php7.2-mysql mysql-server
+apt-get install -y php-curl php-gd php-intl php-mbstring php-soap php-xml php-xmlrpc php-zip
 
 #configure file wordpress
 curl -LO https://wordpress.org/latest.tar.gz
-sudo tar xzvf /latest.tar.gz
-curl -s https://api.wordpress.org/secret-key/1.1/salt/ > /auth.txt
+tar xzvf /latest.tar.gz
+#curl -s https://api.wordpress.org/secret-key/1.1/salt/ > /auth.txt
 cp /wordpress/wp-config-sample.php /wordpress/wp-config.php
-sudo chown -R ubuntu:ubuntu /wordpress /auth.txt
+#sudo chown -R ubuntu:ubuntu /wordpress /LEMP-Wp-master/auth.txt
 sed -i '49,56d' /wordpress/wp-config.php
-sed -i '48r auth.txt' /wordpress/wp-config.php
+sed -i '48r \/LEMP-Wp-master\/auth.txt' /wordpress/wp-config.php
 sed -i 's/database_name_here/nama-db/g' /wordpress/wp-config.php
 sed -i 's/username_here/nama-pengguna/g' /wordpress/wp-config.php
 sed -i 's/password_here/katasandi/g' /wordpress/wp-config.php
@@ -27,8 +27,8 @@ rm -f /etc/nginx/sites-available/default
 rm -f /etc/nginx/sites-enabled/default
 cp /LEMP-Wp-master/wordpress.conf /etc/nginx/sites-available
 ln -s /etc/nginx/sites-available/wordpress.conf /etc/nginx/sites-enabled/
-sudo systemctl restart php7.2-fpm
-sudo systemctl restart nginx.service
+systemctl restart php7.2-fpm
+systemctl restart nginx.service
 
 #configure mysql
 mysql -h alamat -u nama-pengguna -p"katasandi" -e "CREATE DATABASE nama-db;"
